f5dcc6f75df48ef46279ec8ff50c95fad70f3472
pridapablo <pridapablo@gmail.com>
2022-04-29 14:00:10 -0500

    create base interface for SP

---
+++ SP/src/movies.cpp
@@ -0,0 +1,12 @@
+// Importar datos usando std::fstream = 3 pt extras
+// ifstream es una clase que permite crear la instancia fich para leer de un fichero
+
+#include <iostream>
+#include "movies.h"
+#define USING_MAIN
+
+#ifdef USING_MAIN
+int main() {
+    std::cout << "holi";
+}
+#endif
---
+++ SP/src/movies.h
@@ -0,0 +1,29 @@
+class ServicioDeStreaming{
+//private:
+};
+
+class Video{
+//private:
+    int ID;
+    std::string nombre;
+    std::chrono::minutes duracion;
+    std::string genero;
+
+public:
+    Video(){}
+
+};
+
+class Pelicula{
+//private:
+};
+
+class Serie{
+//private:
+};
+
+class Episodio{
+//private:
+    std::string titulo;
+    int temporada;
+};
